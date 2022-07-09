788f0448eb0c6d8f9a1fe69f78e564d7f2044b19
nocotymowisz <kamil.lukasik.91@gmail.com>
2022-07-09 10:22:29 +0200

    Page Object Pattern - first tests

---
+++ src/test/java/pl/coderslab/seleniumcourse/pageObject/MainDdgPage.java
@@ -0,0 +1,20 @@
+package pl.coderslab.seleniumcourse.pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MainDdgPage {
+    private WebDriver driver;
+
+    public MainDdgPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void searchPhrase(String phraseToSearch) {
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys(phraseToSearch);
+        searchInput.submit();
+    }
+}
+
