b6e590c125b4f26318edeb15f2b23e9bb07410ef
nocotymowisz <kamil.lukasik.91@gmail.com>
2022-07-09 16:37:26 +0200

    hotel search

--- src/test/java/pl/coderslab/seleniumcourse/pageObject/CreateAnAccountHotelTestlabPage.java
+++ src/test/java/pl/coderslab/seleniumcourse/pageObject/CreateAnAccountHotelTestlabPage.java
@@ -3,43 +3,43 @@ package pl.coderslab.seleniumcourse.pageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import static pl.coderslab.seleniumcourse.Tools.fillInput;
 
 public class CreateAnAccountHotelTestlabPage {
     private WebDriver driver;
 
+    @FindBy(id = "customer_firstname")
+    private WebElement customerFirstnameInput;
 
+    @FindBy(id = "customer_lastname")
+    private WebElement customerLastnameInput;
 
-    private By customerFirstnameLocator = By.id("customer_firstname");
+    @FindBy(id = "passwd")
+    private WebElement passwordInput;
 
-    private By customerLastNameLocator = By.id("customer_lastname");
+    @FindBy(id = "submitAccount")
+    private WebElement submitButton;
 
-    private By customerPasswordLocator = By.id("passwd");
-
-    private By submitButtonLocator = By.id("submitAccount");
+    @FindBy(id = "email")
+    private WebElement emailInput;
 
     public CreateAnAccountHotelTestlabPage(WebDriver driver) {
-
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void attemptAccountCreationForUserDetails(UserDetailsDto userDetails) {
-        WebElement customerFirstnameInput = driver.findElement(customerFirstnameLocator);
         fillInput(customerFirstnameInput, userDetails.getFirstName());
-        WebElement customerLastnameInput = driver.findElement(customerLastNameLocator);
         fillInput(customerLastnameInput, userDetails.getLastName());
-        WebElement passwordInput = driver.findElement(customerPasswordLocator);
         fillInput(passwordInput, userDetails.getPassword());
 
-        WebElement submitButton = driver.findElement(submitButtonLocator);
         submitButton.click();
     }
 
     public boolean areMandatoryInputsInteractable() {
-        WebElement customerFirstnameInput = driver.findElement(customerFirstnameLocator);
-        WebElement customerLastnameInput = driver.findElement(customerLastNameLocator);
-        WebElement passwordInput = driver.findElement(customerPasswordLocator);
         boolean isFirstNameInputInteractable = customerFirstnameInput.isDisplayed() && customerFirstnameInput.isEnabled();
         boolean isLastNameInputInteractable = customerLastnameInput.isDisplayed() && customerLastnameInput.isEnabled();
         boolean isPasswordInputInteractable = passwordInput.isDisplayed() && passwordInput.isEnabled();
@@ -50,7 +50,6 @@ public class CreateAnAccountHotelTestlabPage {
     }
 
     public String getUserEmail() {
-        WebElement emailInput = driver.findElement(By.id("email"));
         return emailInput.getAttribute("value");
     }
 }
